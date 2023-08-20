06cacf8db358f4a27998860a910ed56e86c3b42d
Saaleh <97705117+SPoovathumkadavil@users.noreply.github.com>
2023-08-20 16:33:18 -0500

    Initial Commit

---
+++ SallyNotes/SallyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SallyNotesApp.swift
+//  SallyNotes
+//
+//  Created by Saaleh Poovathumkadavil on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SallyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
